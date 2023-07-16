52b69be7ae2f2143d4794f5f6a5daff2901e19bb
Jorge Luis Moreno Moller <86493703+JMMOLLER@users.noreply.github.com>
2023-07-15 20:51:21 -0500

    Update index.js
    Se añadió un texto más.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 const typeAnimation = new TypeAnimation('#toType', {
-    strings: ['Desarrollador Frontend', 'Desarrollador Backend', 'Desarrollador Fullstack'],
+    strings: ['Desarrollador Frontend', 'Desarrollador Backend', 'Desarrollador Fullstack', 'Ingeniero de Software'],
     typeSpeed: 150,
     backSpeed: 100,
     delayBtwStrings: 2000,
